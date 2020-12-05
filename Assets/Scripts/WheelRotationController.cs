9767f28e5708e53234a73abb561f7e82018c8a2a
Probal <probal.kumar.das@gmail.com>
2020-12-05 18:26:24 -0500

    Removed WheelRotationController from wheel object and added to button click event

--- Assets/Scripts/WheelRotationController.cs
+++ Assets/Scripts/WheelRotationController.cs
@@ -5,15 +5,17 @@ using UnityEngine;
 public class WheelRotationController : MonoBehaviour
 {
     float rotSpeed = 0;
+    public GameObject wheel;
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            this.rotSpeed = 10;
-        }
-        transform.Rotate(this.rotSpeed, 0, 0);
+        wheel.transform.Rotate(this.rotSpeed, 0, 0);
 
         this.rotSpeed *= 0.99f;
+    }
+
+    public void RotateWheel()
+    {
+        this.rotSpeed = 10;
     }
 }
