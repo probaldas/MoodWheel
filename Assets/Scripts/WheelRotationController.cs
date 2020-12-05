b9754c4e3a8ed484bccfe071f70af6d6829c3a37
Probal <probal.kumar.das@gmail.com>
2020-12-05 17:39:03 -0500

    Added WheelRotationController script

---
+++ Assets/Scripts/WheelRotationController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelRotationController : MonoBehaviour
+{
+    float rotSpeed = 0;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            this.rotSpeed = 10;
+        }
+        transform.Rotate(this.rotSpeed, 0, 0);
+
+        this.rotSpeed *= 0.99f;
+    }
+}
